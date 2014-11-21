91e89ce040b334fa9681513515526266ddb3fb14
mattcoady <mattcoady@gmail.com>
2014-11-21 11:45:50 -0800

    Move all JS to file, Fix Terminology

--- js/scoring.js
+++ js/scoring.js
@@ -1,38 +1,50 @@
-var rootRef = new Firebase("https://unpong.firebaseio.com");
-var activeGameRef = new Firebase("https://unpong.firebaseio.com/liveGame");
-var activeGameScoreRef = new Firebase("https://unpong.firebaseio.com/liveGame/score");
+var activeGameScoreRef = new Firebase ("https://unpong.firebaseio.com/liveGame/score");
 
-$('.left, .right').click(function () {
-    self = $(this);
-    self.addClass('pressed');
-    setTimeout(function () {
-        self.removeClass('pressed');
+var scoreControl = true;
+var scoreViewer = true;
+
+//Function if Screen is Score Control
+var initScoreControl = function () {
+  $ ('.view-team0, .view-team1').click (function () {
+    self = $ (this);
+    self.addClass ('pressed');
+    setTimeout (function () {
+      self.removeClass ('pressed');
     }, 200);
-    console.log('blah');
-    console.log(self.attr('class'));
-    if (self.attr('data-player') === "0") {
-        checkScore(0);
-    } else if (self.attr('data-player') === "1") {
-        checkScore(1);
+    if (self.attr ('data-player') === "0") {
+      checkScore (0);
+    } else if (self.attr ('data-player') === "1") {
+      checkScore (1);
     }
-});
+  });
 
-var checkScore = function (side) {
-    activeGameScoreRef.once("value", function (snapshot) {
-        setScore(side, snapshot.val());
+  var checkScore = function (side) {
+    activeGameScoreRef.once ("value", function (snapshot) {
+      setScore (side, snapshot.val ());
     }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
+      console.log ("The read failed: " + errorObject.code);
     });
-};
+  };
 
-var setScore = function (side, currentScore) {
+  var setScore = function (side, currentScore) {
     if (side === 0) {
-        newScore = currentScore.team0 + 1;
-        activeGameScoreRef.update({team0: newScore});
+      newScore = currentScore.team0 + 1;
+      activeGameScoreRef.update ({team0: newScore});
     } else if (side === 1) {
-        newScore = currentScore.team1 + 1;
-        activeGameScoreRef.update({team1: newScore});
+      newScore = currentScore.team1 + 1;
+      activeGameScoreRef.update ({team1: newScore});
     }
+  };
 };
 
+//Function if Screen is Score/Leaderboard
+var initScoreViewer = function () {
+  activeGameScoreRef.on ('value', function (snapshot) {
+    $ ('.score-card.team0').text (snapshot.val ().team0);
+    $ ('.score-card.team1').text (snapshot.val ().team1);
+  });
+};
+
+if (scoreControl)initScoreControl();
+if (scoreViewer)initScoreViewer();
 
