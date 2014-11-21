71b467c4313a03a19fff4721ba583f9ef02e17e0
Arthur Gonigberg <artgon@gmail.com>
2014-11-21 11:44:55 -0800

    update firebase version and leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,7 +1,9 @@
 var playerRef = new Firebase("https://unpong.firebaseio.com/player");
 
-playerRef.once("value", function(players) {
-    for(var player in players.val()) {
-        console.log(player);
-    }
+
+playerRef.orderByChild("score").limitToFirst(10).once("value", function(players) {
+    players.val().forEach(function (player) {
+        var playerLine = "<li>" + player.name + " <span class=\"score\">" + player.score + "</span></li>";
+        $('.leaderboard').append(playerLine);
+    });
 });
