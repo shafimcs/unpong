d6f1c82181cc1ef8b0b18eeb16bd140c7412ea41
Arthur Gonigberg <artgon@gmail.com>
2014-11-21 12:12:23 -0800

    take leaders manually

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,14 +1,14 @@
 var playerRef = new Firebase("https://unpong.firebaseio.com/player");
 
 
-playerRef.limitToFirst(10).once("value", function (players) {
+playerRef.once("value", function (players) {
     var playerMap = _(players.val());
 
     var sortedPlayers = _.sortBy(playerMap.values(), function (player) {
         return -player.score;
     });
 
-    sortedPlayers.forEach(function (player) {
+    _(sortedPlayers).first(8).forEach(function (player) {
         var playerLine = "<li>" + player.name + " <span class=\"score\">" + player.score + "</span></li>";
         $('.leaderboard').append(playerLine);
     });
