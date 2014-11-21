bf3b05f7249e65918bf086dde60ccfeedcc0628f
Arthur Gonigberg <artgon@gmail.com>
2014-11-21 12:00:30 -0800

    leaderboard with sorting

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,8 +1,14 @@
 var playerRef = new Firebase("https://unpong.firebaseio.com/player");
 
 
-playerRef.orderByChild("score").limitToFirst(10).once("value", function(players) {
-    players.val().forEach(function (player) {
+playerRef.limitToFirst(10).once("value", function (players) {
+    var playerMap = _(players.val());
+
+    var sortedPlayers = _.sortBy(playerMap.values(), function (player) {
+        return -player.score;
+    });
+
+    sortedPlayers.forEach(function (player) {
         var playerLine = "<li>" + player.name + " <span class=\"score\">" + player.score + "</span></li>";
         $('.leaderboard').append(playerLine);
     });
